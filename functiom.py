d80495ca53915a1bd17b62c8246cb0d44203d17d
osarenomawise <erhaboreducation@gmail.com>
2022-09-15 15:45:54 +0800

    this is another commit on function

--- functiom.py
+++ functiom.py
@@ -1,2 +1,5 @@
 def epmployee(name: str, age: int, salary: float):
     print(f'my is {name} and i\'m  {age} and my salary is {salary}')
+
+def another_function():
+    print('hello world this is another function')
